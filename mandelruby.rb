c927ad4aec2fa91aad4580f9d6fd1a470a7bfbff
Dominic Muller <nicklink483@gmail.com>
2014-07-20 22:45:06 -0700

    refactored Set class

--- mandelruby.rb
+++ mandelruby.rb
@@ -1,7 +1,4 @@
 #Mandelbrot Set graphical generator
 require_relative 'mandelruby/set'
 
-set = Mandelruby::Set.new.init
-puts set.draw
-
-puts Mandelruby::Set.new.draw_2
+puts Mandelruby::Set.new.draw
