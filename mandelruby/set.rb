3a86d9333f5d55db8003baf1dd9114da8d263403
Luke Morris <luke@imfuna.com>
2014-07-22 13:32:50 -0700

    Begin to encapsulate mandelbrot calculation in Mandelbrot class.

--- mandelruby/set.rb
+++ mandelruby/set.rb
@@ -67,6 +67,63 @@ module Mandelruby
 
   end
 
+  class Mandelbrot
+    def initialize
+      # maximum iterations the recursive loop tries before bailing out and
+      # considers a point in the set
+      @dwell = 100
+      #how many iterations before output uses a different character for bail levels
+      @character_resolution = 2
+      # chooses a number between 15 and 240 for the starting color
+      @bg_color = rand(15..240)
+      @fg_color = rand(15..240)
+      # only display color output if --color option passed
+      @color = color
+    end
+
+    def mandelbrot(c)
+      z = 0
+      @iteration = 0
+      @dwell.times { z = z**2 + c; @iteration += 1; break if (z.abs >= 2)}
+      character_for_iteration
+    end
+
+    def character_for_iteration
+      return " " if @iteration == @dwell
+      
+      if @color
+        color_for_iteration + (char_list[@iteration/@character_resolution] || ".") + reset_color
+      else
+        char_list[@iteration/@character_resolution] || "."
+      end
+    end
+
+    def char_list
+      @char_list ||= ["X","O","#","*","o","%","=","-","."]
+    end
+
+    def color_for_iteration
+      bg_color + fg_color
+    end
+
+    def bg_color
+      ["\e[48;5;", (@bg_color + color_index), "m"].join
+    end
+
+    def fg_color
+      ["\e[38;5;", (@fg_color + color_index), "m"].join
+    end
+
+    def reset_color
+      "\e[0m"
+    end
+
+    def color_index
+      char_list.index(char_list[@iteration/@character_resolution]) || 8
+    end
+
+  end
+
   class Window
     attr_reader :top_left, :bottom_right, :resolution
     def initialize
