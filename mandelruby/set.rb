ffd8d172869d0c9a285044a6866a9048ba0d2bf4
Luke Morris <luke@imfuna.com>
2014-07-22 15:04:13 -0700

    Start to move classes into different files.

--- mandelruby/set.rb
+++ mandelruby/set.rb
@@ -1,3 +1,5 @@
+require_relative 'crayons'
+
 module Mandelruby
 
   class Set
@@ -91,40 +93,6 @@ module Mandelruby
       calculator.calculate(Complex(x,y))
     end
 
-  end
-
-  class Crayon
-
-    attr_reader :background, :foreground
-    def initialize
-      @background = rand(15..240)
-      @foreground = rand(15..240)
-    end
-
-    def color_in(character, offset)
-      bg_color(offset) + fg_color(offset) + character + reset_color
-    end
-
-    private
-    
-    def bg_color(offset)
-      ["\e[48;5;", (background + offset), "m"].join
-    end
-
-    def fg_color(offset)
-      ["\e[38;5;", (foreground + offset), "m"].join
-    end
-
-    def reset_color
-      "\e[0m"
-    end
-
-  end
-
-  class NullCrayon
-    def color_in(character, *args)
-      character
-    end
   end
 
 end
